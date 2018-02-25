fe133ec1e5fdeadda109a1c4f2b74833e71d8143
JoeSelvik <joe@thenewtricks.com>
2018-02-25 14:23:12 -0700

    organize tests

--- test/NickelbackCoin.test.js
+++ test/NickelbackCoin.test.js
@@ -1,5 +1,8 @@
+// request a usable contract abstraction for a specific Solidity contract
 const NickelbackCoin = artifacts.require("NickelbackCoin");
 
+// before each contract() function, your contracts are redeployed to the running Ethereum
+// client so the tests within it run with a clean contract state
 contract("NickelbackCoin", accounts => {
   let nbc;
   const ETH = 1000000000000000000;
@@ -23,9 +26,9 @@ contract("NickelbackCoin", accounts => {
   });
 
   it("assigns the initial total supply to the creator", async function() {
-    const totalSupply = await nbc.totalSupply();
-    const creatorBalance = await nbc.balanceOf(creator);
     const initialSupply = await nbc.INITIAL_SUPPLY();
+    const creatorBalance = await nbc.balanceOf(creator);
+    const totalSupply = await nbc.totalSupply();
 
     assert(creatorBalance.eq(initialSupply));
     assert(totalSupply.eq(creatorBalance));
@@ -33,20 +36,27 @@ contract("NickelbackCoin", accounts => {
 
   describe("funding the contract", function() {
     beforeEach(async function() {
-      nbc.sendTransaction({
+      // Fails - all sendTransaction's tx exit with error
+      await nbc.sendTransaction({
         from: donor,
         value: web3.toWei("0.1", "ether")
       });
     });
 
+    console.log("hello");
+
     it("does not change user balances", async function() {
-      var bigNumber = await nbc["balanceOf"](creator);
-      const creatorBalance = await bigNumber.toNumber();
+      console.log("1");
+      let creatorBalance = await nbc["balanceOf"](creator).toNumber();
       assert.equal(creatorBalance, 400);
 
-      // bigNumber = await nbc["balanceOf"](user);
-      // const userBalance = await bigNumber.toNumber();
-      // assert.equal(userBalance, 0);
+      console.log("2");
+      let userBalance = await nbc["balanceOf"](user).toNumber();
+      assert.equal(userBalance, 0);
+
+      console.log("3");
+      let donorBalance = await nbc["balanceOf"](donor).toNumber();
+      console.log(donorBalance);
     });
   });
 });
